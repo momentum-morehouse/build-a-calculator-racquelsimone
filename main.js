b5e32335cbf304882037c5dae8b3eeca03a0c67b
Racquel WS <racquelwsf@gmail.com>
2020-06-12 15:08:23 +0000

    Copied into new repl due to repl crashing last night. Almost finished; unable to figure out getting numbers to display. So close!!

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+function getButton() {
+  let buttons = document.querySelectorAll(".btn");
+  console.log(buttons);
+  let display = document.querySelector("#displayScreen");
+  for (let button of buttons) {
+    console.log(button.textContent);
+    button.addEventListener("click", function () {
+      alert(buttons);
+    })
+      ;
+  }
 
+}
+getButton();
